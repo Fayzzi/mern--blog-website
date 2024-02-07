769b9f751e00d7cc3ed5b3f42b81e139f62a82b5
Fayzzi <faizancuiwah@gmail.com>
2024-02-07 21:51:22 +0500

    User Model and error Middlewares

---
+++ backend/Models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+//hashing password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+//getting jwtToken
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_TOKEN_SECRET, {
+    expiresIn: process.env.JWT_TOKEN_DURATION,
+  });
+};
+//comparing passwords
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+module.exports = mongoose.model("blog-user", userSchema);
