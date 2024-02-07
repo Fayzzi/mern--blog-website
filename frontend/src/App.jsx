62a3f90ef5d5dd9ecf72615b684a6f75ac5a59b3
Fayzzi <faizancuiwah@gmail.com>
2024-02-07 14:25:29 +0500

    craete pages and Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/HomePage/Home";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import Dashboard from "./Pages/Dashboard/Dashboard";
 function App() {
-  return <div className="bg-black">Ok boss</div>;
+  return (
+    <Routes>
+      <Route element={<Home />} path="/" />
+      <Route element={<Login />} path="/login" />
+      <Route element={<Signup />} path="/signup" />
+      <Route element={<Dashboard />} path="/dashboard" />
+    </Routes>
+  );
 }
 
 export default App;
